4a9394ed1c0e706eb01461bb0c6091c225045539
Zachary Smith <zdavsmith@gmail.com>
2019-08-17 19:02:21 -0400

    Add do and while statements

---
+++ Tech_Acad_Loop_Practice/Tech_Acad_Loop_Practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Tech_Acad_Loop_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practicing Switch Satements
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12; //If number == 12, then its true, else, isGuessed is false
+
+
+            do //The do makes sure the loop itterates through at least once. (Also, the do and while are connected
+            {
+                switch (number)
+                {
+                    // Revolves around cases -- Ex: Did this happen? Did this Happen?
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You're wrong!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed); // ! means the same as -- is false. So "while isGuessed = false"
+            Console.Read();
+        }
+    }
+}
---
+++ While_And_Do_While_Statements_Tech_Proj/While_And_Do_While_Statements_Tech_Proj/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace While_And_Do_While_Statements_Tech_Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //First while statement 
+            Console.WriteLine("Type in a number between 1 and 10");
+            int mainNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number total = " + mainNum);
+
+
+            while (mainNum < 30)
+            {
+            Console.WriteLine("Now type in another number between 1 and 10. Continue doing so until your number surpasses 30");
+            int changingNum = Convert.ToInt32(Console.ReadLine());
+            mainNum = changingNum + mainNum;
+            Console.WriteLine("Your number total = " + mainNum);
+            }
+            Console.WriteLine("Congratulations, your number is now equal to or greater than the value of 30");
+
+            Console.WriteLine("Now, I am thinking of a number between 1 and 10. Please guess a number: ");
+            int numGuessed = Convert.ToInt32(Console.ReadLine());
+            bool correctAnswer = numGuessed == 7;
+            do
+            {
+            switch(numGuessed)
+                {
+                    case 7:
+                        Console.WriteLine("That is the correct answer! Congratulations");
+                        correctAnswer = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Whoops, sorry! That is incorrect. Please guess again: ");
+                        numGuessed = Convert.ToInt32(Console.ReadLine());
+                   break;
+                }
+            }
+            while (!correctAnswer);
+            Console.Read();
+        }
+    }
+}
