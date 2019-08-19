07ae643ece32ea8394af788e9b1127f4f01a6008
Zachary Smith <zdavsmith@gmail.com>
2019-08-19 13:00:39 -0400

    Separate Files by Prj and Tutorial
    Organize File locations and add string concatenation practice

---
+++ Basic_Tutorials_Folder/String_Practice_Tech_Proj/String_Practice_Tech_Proj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace String_Practice_Tech_Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            string quote = "The man said, \"Hello\", Jesse. \n Hello on a new line.";
+            // string fileName = @"C:\Users\Jesse"; //The @ tells the compilers that everything between the quotes is a part of the main string. No escapes
+
+            // bool trueOrFalse = name.Contains("s");// both of these are helpful when looking for a specific character
+            // trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length; // Finds the number of characters
+
+            // name = name.ToUpper(); //Converts everything to uppercase..
+            // name = name.ToLower(); // Converts everything to lower
+
+            //Console.WriteLine(length);
+            //Console.ReadLine();
+
+
+
+            //Strings are immutable. Once you create them, you cant change them
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
