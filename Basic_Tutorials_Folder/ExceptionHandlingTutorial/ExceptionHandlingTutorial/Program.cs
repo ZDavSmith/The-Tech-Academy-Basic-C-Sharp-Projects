62c67cfd3049f79028d98fe28483ce4fefd4d0e6
Zachary Smith <zdavsmith@gmail.com>
2019-08-21 16:41:22 -0400

    Add debugging projects

---
+++ Basic_Tutorials_Folder/ExceptionHandlingTutorial/ExceptionHandlingTutorial/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //==============================================================================================
+        //When something goes wrong in the code, Exception Handling, that checks and responds to errors
+        //===============================================================================================
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+
+        }
+        catch( FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number."); //Format Exception
+            return; //Brings User to different Page
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by 0"); //DividebyZero
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message); //General
+        }
+        finally //Will run no matter what, whether or not there is an exception. Prevents the stop in exe. Usually you'll have a database log in your finally
+        {
+            Console.ReadLine();
+        }
+       
+
+
+
+
+        }
+    }
+
---
+++ Project_Folder/Debugging_Applications_Project/Debugging_Applications_Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Debugging_Applications_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> integerList = new List<int> { 42, 2, 178, 90, 73 };
+                Console.WriteLine("Please type in a number");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                foreach (int num in integerList)
+                {
+                    int dividedNum = num / userNum;
+                    Console.WriteLine(num + " divided by your number of " + userNum + " = " + dividedNum);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a non-zero number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please do not divide by 0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please read the instructions carefully and try again");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
